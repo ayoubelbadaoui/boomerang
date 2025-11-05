0dd6f2c416fd214982f0e35b8249454f0b5721f4
Ayoub <mac@Macs-MacBook-Pro.local>
2025-11-05 02:27:20 +0100

    chore(appid): update bundle ids\n\n- Android applicationId/namespace: com.alwa.boomerang (lowercase per Android package rules)\n- iOS PRODUCT_BUNDLE_IDENTIFIER: Com.alwa.boomerang (+ RunnerTests)\n\nNote: update google-services.json and Firebase console package if needed.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.digitalnova.boomerang.boomerang"
+    namespace = "com.alwa.boomerang"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.digitalnova.boomerang.boomerang"
+        applicationId = "com.alwa.boomerang"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
---
+++ android/app/src/main/kotlin/com/alwa/boomerang/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.alwa.boomerang
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
+
+
